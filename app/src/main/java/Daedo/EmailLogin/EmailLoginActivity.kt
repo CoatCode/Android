d58a12f15e4e31b0a58c38fadf7324a44912d1bd
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-08-09 00:36:35 +0900

    안드로이드 첫화면과 이메일 로그인 레이아웃

---
+++ app/src/main/java/Daedo/EmailLogin/EmailLoginActivity.kt
@@ -0,0 +1,19 @@
+package Daedo.EmailLogin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.junhyuk.daedo.MainActivity
+import com.junhyuk.daedo.R
+import kotlinx.android.synthetic.main.activity_email_login.*
+
+class EmailLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_login)
+        x_button.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/junhyuk/daedo/MainActivity.kt
+++ app/src/main/java/com/junhyuk/daedo/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.junhyuk.daedo
 
-import androidx.appcompat.app.AppCompatActivity
+import Daedo.EmailLogin.EmailLoginActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        email_login_button.setOnClickListener {
+            startActivity(Intent(this, EmailLoginActivity::class.java))
+            finish()
+        }
+
+
+
     }
+
 }
