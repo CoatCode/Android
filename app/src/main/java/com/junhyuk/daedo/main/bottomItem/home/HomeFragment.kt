3df2ff56013b211abe78499c494ce8ad783f0317
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-10-12 20:48:29 +0900

    [Dvelopment]BottomSheetDialog
    바텀시트 dialog 구현

--- app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/HomeFragment.kt
+++ app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/HomeFragment.kt
@@ -1,24 +1,27 @@
 package com.junhyuk.daedo.main.bottomItem.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.junhyuk.daedo.R
 import com.junhyuk.daedo.feed.getCommentList.CommentData
+import com.junhyuk.daedo.feed.getCommentList.GetCommentList
 import com.junhyuk.daedo.feed.getCommentList.PersonAdapter
+import com.junhyuk.daedo.feed.writeComment.SendWriteComment
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
-//    var photo : String? = Owner.instance!!.profile
-//    var name : String? = Owner.instance!!.username
-////    var email : String? = Owner.instance!!.email
+    var sibal : String = "asd"
     private var personList = arrayListOf<CommentData>(
-//        //recyclerview 에 담길 값
-        CommentData("강민석", "애플망해라"),
+        //recyclerview 에 담길 값
+        CommentData(sibal, "애플망해라"),
         CommentData("강민석", "애플웩"),
         CommentData("강민석", "애플구려"),
         CommentData("강민석", "애플별로야"),
@@ -29,7 +32,7 @@ class HomeFragment : Fragment() {
         CommentData("강민석", "갤럭시 s20살래")
     )
 
-  //  private var comment: String = ""
+    private var comment: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,19 +46,19 @@ class HomeFragment : Fragment() {
         view.comment_recycler_view.setHasFixedSize(true)
         view.comment_recycler_view.layoutManager = lm
 
-//        val sendComment = SendWriteComment()
-//        val getComment = GetCommentList()
-//        val ct: FragmentActivity? = activity
+        val sendComment = SendWriteComment()
+        val getComment = GetCommentList()
+        val ct: FragmentActivity? = activity
         //댓글 작성 버튼 누를 시 SendComment 클래스 호출하고 입력받은 댓글을 넘겨준다
-//        //view.write_comment?.setOnClickListener {
-//            comment = this.edit_comment.text.toString()
-//            Log.d("test","test:$comment")
-//            sendComment.sendComment(comment, ct!!.application)
-//            getComment.getCommentList(ct.application)
-//        //}
+        view.write_comment?.setOnClickListener {
+            comment = this.edit_comment.text.toString()
+            Log.d("test", "test:$comment")
+            sendComment.sendComment(comment, ct!!.application)
+            getComment.getCommentList(ct.application)
+            }
 
 
-        return view
+            return view
+        }
+
     }
-
-}
