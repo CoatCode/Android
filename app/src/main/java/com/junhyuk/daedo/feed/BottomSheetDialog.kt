e1ee1ed2ac5aeb81b5ace196d61a7bcfeca69234
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-10-22 20:47:52 +0900

    [Development]CorrectComment
    댓글 수정 기능 개발 중

--- app/src/main/java/com/junhyuk/daedo/feed/BottomSheetDialog.kt
+++ app/src/main/java/com/junhyuk/daedo/feed/BottomSheetDialog.kt
@@ -1,8 +1,7 @@
 package com.junhyuk.daedo.feed
 
-import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +9,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.junhyuk.daedo.R
+import com.junhyuk.daedo.feed.correctComment.CorrectComment
 import com.junhyuk.daedo.feed.deleteComment.DeleteComment
 import com.junhyuk.daedo.feed.getCommentNetwork.CommentData
 import kotlinx.android.synthetic.main.bottom_sheet_layout.view.*
@@ -26,6 +26,9 @@ class BottomSheetDialog(Comment: CommentData, view: View?) : BottomSheetDialogFr
     private val commentContent = Comment.content
     private val fragmentHomeView = view
     private val comment = Comment
+    private val correctComment = CorrectComment()
+    private val activityBox = requireActivity()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,27 +42,35 @@ class BottomSheetDialog(Comment: CommentData, view: View?) : BottomSheetDialogFr
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val editComment: EditText? = fragmentHomeView?.findViewById(R.id.edit_comment)
-        val activityBox = requireActivity()
-        val bottomSheet = BottomSheetDialog(comment,view)
+
+        val bottomSheet = BottomSheetDialog(comment, view)
+        val dia = dialog
+
+
         //댓글 수정 버튼
         view.comment_correct.setOnClickListener {
-            // correctComment.correctComment(activityBox.application, commentContent)
-            editComment?.showKeyboard()
+            correctComment.correctComment(activityBox.application, commentContent)
+            //editComment?.showKeyboard()
             editComment?.setText(commentContent)!!
-
+            if (dia != null) {
+                dismissAllowingStateLoss()
+            }
+            editComment.showSoftInputOnFocus
         }
+
         //댓글 삭제 버튼
         view.comment_delete?.setOnClickListener {
-            bottomSheet.dismiss()
-            deleteComment.deleteComment(activityBox.application, commentId)
-            Log.d("example","example")
+            bottomSheet.deleteComment.deleteComment(activityBox.application, commentId)
+            val imm = context?.getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(editComment?.getWindowToken(), 0)
+            if (dia != null) {
+                dismissAllowingStateLoss()
+            }
+
         }
     }
-
-    private fun EditText.showKeyboard() {
-            requestFocus()
-            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-            Log.d("example","example : $imm")
+    private fun correct(){
+        correctComment.correctComment(activityBox.application,commentId)
     }
+
 }
