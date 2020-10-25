f4752a0f00b4822b928ca8d7b1659766bc0dd341
YJH <chuchoun1234@gmail.com>
2020-10-25 15:03:04 +0900

    [UPDATE] adpater와 like modul 연결e

--- app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/adapter/FeedAdapter.kt
+++ app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/adapter/FeedAdapter.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -15,40 +17,91 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.junhyuk.daedo.R
 import com.junhyuk.daedo.main.bottomItem.home.data.FeedData
 import com.junhyuk.daedo.main.bottomItem.home.module.FeedTime
+import com.junhyuk.daedo.main.bottomItem.home.module.LikeClickModule
+import com.junhyuk.daedo.main.bottomItem.home.module.LikeModule
 import com.junhyuk.daedo.main.bottomItem.home.paging.DiffUtilCallBack
 import kotlinx.android.synthetic.main.cardview_feed_item.view.*
 
 
-class FeedAdapter(private val context: Context) : PagedListAdapter<FeedData, FeedAdapter.Holder>(DiffUtilCallBack()) {
+class FeedAdapter(private val context: Context) :
+    PagedListAdapter<FeedData, FeedAdapter.Holder>(DiffUtilCallBack()) {
 
     private val feedPostTime = FeedTime()
+    private val likeClickModule = LikeClickModule()
+    private val likeModule = LikeModule()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_feed_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.cardview_feed_item, parent, false)
         return Holder(view)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val feedData = getItem(position)
 
-        if(feedData != null){
-            Glide.with(context)
-                .load(feedData.owner.image)
-                .transform(CenterCrop(), RoundedCorners(1000000))
-                .into(holder.profileImageView)
+        holder.itemView.setOnClickListener {
 
-            Glide.with(context)
-                .load(feedData.image_urls[0])
-                .into(holder.feedImage)
+        }
 
-            Log.d("feedTime", "data: ${System.currentTimeMillis()}")
+        likeModule.likeModule(context, feedData?.id!!, holder)
 
-            holder.userName.text = feedData.owner.username
-            holder.feedTime.text = feedPostTime.calFeedTime(feedData.created_at)
-            holder.title.text = feedData.title
-            holder.heartCount.text = feedData.like_count.toString()
-            holder.commentCount.text = feedData.comment_count.toString()
-            holder.viewerCount.text = feedData.view_count.toString()
+        holder.heartButton.setOnClickListener {
+            likeClickModule.likeClickModule(context, feedData.id, holder)
+        }
+
+        Glide.with(context)
+            .load(feedData.owner.image)
+            .override(100)
+            .transform(CenterCrop(), RoundedCorners(1000000))
+            .into(holder.profileImageView)
+
+        Glide.with(context)
+            .load(feedData.image_urls[0])
+            .into(holder.feedImage)
+
+        Log.d("feedTime", "data: ${System.currentTimeMillis()}")
+
+        holder.userName.text = feedData.owner.username
+        holder.feedTime.text = feedPostTime.calFeedTime(feedData.created_at)
+        holder.title.text = feedData.title
+        holder.heartCount.text = feedData.like_count.toString()
+        holder.commentCount.text = feedData.comment_count.toString()
+        holder.viewerCount.text = feedData.view_count.toString()
+
+        when {
+            feedData.comment_count <= 0 -> {
+                holder.commentView.visibility = View.GONE
+                holder.commentView2.visibility = View.GONE
+            }
+            feedData.comment_count == 1 -> {
+                holder.commentView.visibility = View.VISIBLE
+                Glide.with(context)
+                    .load(feedData.comment_preview[0].owner.image)
+                    .transform(CenterCrop(), RoundedCorners(1000000))
+                    .into(holder.commentProfile)
+                holder.commentName.text = feedData.comment_preview[0].owner.username
+                holder.commentText.text = feedData.comment_preview[0].content
+            }
+            feedData.comment_count > 1 -> {
+
+                holder.commentView.visibility = View.VISIBLE
+                holder.commentView2.visibility = View.VISIBLE
+
+                Glide.with(context)
+                    .load(feedData.comment_preview[0].owner.image)
+                    .transform(CenterCrop(), RoundedCorners(1000000))
+                    .into(holder.commentProfile)
+                holder.commentName.text = feedData.comment_preview[0].owner.username
+                holder.commentText.text = feedData.comment_preview[0].content
+
+                Glide.with(context)
+                    .load(feedData.comment_preview[1].owner.image)
+                    .transform(CenterCrop(), RoundedCorners(1000000))
+                    .into(holder.commentProfile2)
+                holder.commentName2.text = feedData.comment_preview[1].owner.username
+                holder.commentText2.text = feedData.comment_preview[1].content
+
+            }
         }
     }
 
@@ -61,6 +114,15 @@ class FeedAdapter(private val context: Context) : PagedListAdapter<FeedData, Fee
         var commentCount: TextView = itemView.commentCount
         var viewerCount: TextView = itemView.viewerCount
         var feedImage: ImageView = itemView.feedImage
+        var commentProfile: ImageView = itemView.commentProfile
+        var commentProfile2: ImageView = itemView.commentProfile2
+        var commentName: TextView = itemView.commentName
+        var commentName2: TextView = itemView.commentName2
+        var commentText: TextView = itemView.commentContent
+        var commentText2: TextView = itemView.commentContent2
+        var commentView: ConstraintLayout = itemView.commentView
+        var commentView2: ConstraintLayout = itemView.commentView2
+        var heartButton: ImageButton = itemView.heart
     }
 
 }
