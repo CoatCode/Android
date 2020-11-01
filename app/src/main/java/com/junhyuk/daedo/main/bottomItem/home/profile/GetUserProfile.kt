6bfd3634aa2521176c9fdaed10ca735d796b9cb2
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-11-01 22:04:53 +0900

    [Development]UserProfile
    유저 프로필 개발 중

--- app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/profile/GetUserProfile.kt
+++ app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/profile/GetUserProfile.kt
@@ -1,13 +1,37 @@
 package com.junhyuk.daedo.main.bottomItem.home.profile
 
 import android.app.Application
+import android.util.Log
 import com.junhyuk.daedo.application.DaedoApplication
+import com.junhyuk.daedo.main.bottomItem.home.data.PostId
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class GetUserProfile (){
-    internal  fun getUserProfile(
-        application : Application
-    ){
-        var app = (application as DaedoApplication)
+    internal fun getUserProfile(
+        getApplication: Application
+    ) {
 
+        val postId : Int = PostId.postId
+        (getApplication as DaedoApplication).retrofit.create(UserProfileInterface::class.java)
+            .getUserProfile(postId)
+            .enqueue(object : Callback<ArrayList<UserProfileData>> {
+                override fun onResponse(
+                    call: Call<ArrayList<UserProfileData>>,
+                    response: Response<ArrayList<UserProfileData>>
+                ) {
+                    if (response.code() == 200) {
+                        Log.d("responseBody","responseBody${response.body()}")
+                    }
+                    if (response.code() == 401) {
+                        Log.d("401", "401" + response.errorBody())
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<UserProfileData>>, t: Throwable) {
+                    Log.d("fa", "fa")
+                }
+            })
     }
 }
