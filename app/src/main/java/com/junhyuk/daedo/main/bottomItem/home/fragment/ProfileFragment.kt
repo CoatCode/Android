e897397539a2a0c43c5b0f8dd2df5f2b2e92eae6
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-11-03 22:23:31 +0900

    [Development]fragment_profile_layout
    프로필 레이아웃 작업 중

--- app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/fragment/ProfileFragment.kt
+++ app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/fragment/ProfileFragment.kt
@@ -5,15 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.junhyuk.daedo.R
-import com.junhyuk.daedo.main.bottomItem.comment.getCommentList.CommentRecyclerviewAdapter
-import com.junhyuk.daedo.main.bottomItem.comment.getCommentList.GetCommentList
-import com.junhyuk.daedo.main.bottomItem.comment.getCommentList.ItemSize
-import com.junhyuk.daedo.main.bottomItem.comment.getCommentNetwork.CommentData
-import com.junhyuk.daedo.main.bottomItem.comment.writeComment.SendWriteComment
-import kotlinx.android.synthetic.main.fragment_comment.*
-import kotlinx.android.synthetic.main.fragment_comment.view.*
+
 
 class ProfileFragment : Fragment(){
 
@@ -24,7 +17,7 @@ class ProfileFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         val activityBox = activity
-        val view = inflater.inflate(R.layout.fragment_comment, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         return view
     }
