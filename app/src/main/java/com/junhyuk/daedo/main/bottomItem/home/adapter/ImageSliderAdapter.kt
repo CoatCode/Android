cb0dcb96d05777295c84d891dbb2c0f072eb27c2
YJH <chuchoun1234@gmail.com>
2020-11-04 14:56:04 +0900

    [UPDATE] Adapter 수정

--- app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/adapter/ImageSliderAdapter.kt
+++ app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/adapter/ImageSliderAdapter.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.junhyuk.daedo.R
+import com.junhyuk.daedo.main.bottomItem.home.data.ImageData
 import kotlinx.android.synthetic.main.cardview_post_image.view.*
 
 class ImageSliderAdapter(private var images: ArrayList<String>, private val context: Context) :
@@ -25,6 +28,12 @@ class ImageSliderAdapter(private var images: ArrayList<String>, private val cont
         Glide.with(context)
             .load(images[position])
             .into(holder.imagesItem)
+
+        holder.imagesItem.setOnClickListener{
+            ImageData.image = images[position]
+            val navController: NavController = Navigation.findNavController(it)
+            navController.navigate(R.id.action_feedDetailFragment_to_imageDetailFragment)
+        }
     }
 
     override fun getItemCount(): Int {
