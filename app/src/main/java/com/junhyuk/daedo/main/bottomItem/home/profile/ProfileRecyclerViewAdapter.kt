856b34df42f6aefc748d4b2acb99607a491898ce
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-11-09 22:11:31 +0900

    [Add]UserProfile
    유저 프로필 기능 완료(view작업 필요)

--- app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/profile/ProfileRecyclerViewAdapter.kt
+++ app/src/main/java/com/junhyuk/daedo/main/bottomItem/home/profile/ProfileRecyclerViewAdapter.kt
@@ -2,11 +2,13 @@ package com.junhyuk.daedo.main.bottomItem.home.profile
 
 import android.app.Activity
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -14,6 +16,8 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.junhyuk.daedo.R
 import com.junhyuk.daedo.main.bottomItem.comment.getCommentList.CommentRecyclerviewAdapter
 import com.junhyuk.daedo.main.bottomItem.comment.getCommentNetwork.CommentData
+import kotlinx.android.synthetic.main.fragment_profile.*
+import org.w3c.dom.Comment
 
 //recyclerview adapter
 class ProfileRecyclerViewAdapter(private val context: Context, private val postList : ArrayList<UserProfileData>, val view : View?, val userId : Int,
@@ -34,16 +38,27 @@ class ProfileRecyclerViewAdapter(private val context: Context, private val postL
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //유저 게시물1
-        private val userProfile1 = itemView.findViewById<ImageView>(R.id.user_post1).toString()
+        private var userProfile1 = itemView.findViewById<ImageView>(R.id.user_post1)
         //유저 게시물2
-        private val userProfile2 = itemView.findViewById<ImageView>(R.id.user_post2)
+        private var userProfile2 = itemView.findViewById<ImageView>(R.id.user_post2)
         //유저 게시물3
-        private val userProfile3 = itemView.findViewById<ImageView>(R.id.user_post3)
+        private var userProfile3 = itemView.findViewById<ImageView>(R.id.user_post3)
 
 
         fun bind (profile: UserProfileData) {
+            for (i in profile.image_urls.indices){
+                Glide.with(context)
+                    .load(profile.image_urls[i])
+                    .into(userProfile1)
+//                Glide.with(context)
+//                    .load(profile.image_urls[i+1])
+//                    .into(userProfile2)
+//                Glide.with(context)
+//                    .load(profile.image_urls[i+2])
+//                    .into(userProfile3)
+                Log.d("url","url" + profile.image_urls)
+            }
 
-            userProfile1.
         }
     }
 }
